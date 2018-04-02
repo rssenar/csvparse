65cd27d8428058dcc4c190fe24199a5d0fd6f618
Richard Senar <richardsenar@richards-imac.lan>
2018-04-02 15:33:05 -0700

    updates

--- cmd/csvparse/main.go
+++ cmd/csvparse/main.go
@@ -5,12 +5,39 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 
 	"github.com/rssenar/csvparse"
 )
 
+type record struct {
+	Fullname   string    `json:"Full_name" csv:"(?i)^fullname$" fmt:"tc"`
+	Firstname  string    `json:"First_name" csv:"(?i)^first[ _-]?name$" fmt:"tc"`
+	MI         string    `json:"Middle_name" csv:"(?i)^mi$" fmt:"uc"`
+	Lastname   string    `json:"Last_name" csv:"(?i)^last[ _-]?name$" fmt:"tc"`
+	Address1   string    `json:"Address_1" csv:"(?i)^address[ _-]?1?$" fmt:"tc"`
+	Address2   string    `json:"Address_2" csv:"(?i)^address[ _-]?2$" fmt:"tc"`
+	City       string    `json:"City" csv:"(?i)^city$" fmt:"tc"`
+	State      string    `json:"State" csv:"(?i)^state$|^st$" fmt:"uc"`
+	Zip        string    `json:"Zip" csv:"(?i)^(zip|postal)[ _]?(code)?$" fmt:"uc"`
+	Zip4       string    `json:"Zip_4" csv:"(?i)^zip4$|^4zip$" fmt:"uc"`
+	HPH        string    `json:"Home_phone" csv:"(?i)^hph$|^home[ _]phone$" fmt:"fp"`
+	BPH        string    `json:"Business_phone" csv:"(?i)^bph$|^(work|business)[ _]phone$" fmt:"fp"`
+	CPH        string    `json:"Mobile_phone" csv:"(?i)^cph$|^mobile[ _]phone$" fmt:"fp"`
+	Email      string    `json:"Email" csv:"(?i)^email[ _]?(address)?$" fmt:"lc"`
+	VIN        string    `json:"VIN" csv:"(?i)^vin$" fmt:"-"`
+	Year       string    `json:"Year" csv:"(?i)^year$|^vyr$" fmt:"-"`
+	Make       string    `json:"Make" csv:"(?i)^make$|^vmk$" fmt:"tc"`
+	Model      string    `json:"Model" csv:"(?i)^model$|^vmd$" fmt:"tc"`
+	DelDate    time.Time `json:"Delivery_date" csv:"(?i)^del[ ]?date$" fmt:"-"`
+	Date       time.Time `json:"Last_service_date" csv:"(?i)^date$" fmt:"-"`
+	DSFwalkseq string    `json:"DSF_Walk_Sequence" csv:"(?i)^DSF_WALK_SEQ$" fmt:"uc"`
+	CRRT       string    `json:"CRRT" csv:"(?i)^crrt$" fmt:"uc"`
+	KBB        string    `json:"KBB" csv:"(?i)^kbb$" fmt:"uc"`
+}
+
 func main() {
-	var j = flag.Bool("j", false, "Enable output to Indented JSON")
+	// var j = flag.Bool("j", false, "Enable output to Indented JSON")
 	flag.Parse()
 
 	var input io.Reader
@@ -45,27 +72,33 @@ func main() {
 		input = os.Stdin
 	}
 
-	data, err := csvparse.NewDecoder(input).DecodeCSV()
+	// Pass in black []*Record{} container to be filled
+	x := []*record{}
+
+	err := csvparse.NewDecoder(input).DecodeCSV(&x)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	if *j {
-		// MarshalIndent outputs []*Record to JSON for output to os.Stdout
-		// []*Record are stored in the *Parser.Records struct filed
-		err := csvparse.NewEncoder(os.Stdout).EncodeJSON(data)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-	} else {
-		// MarshaltoCSV marshals []*Record to []string for output to os.Stdout
-		// []*Record are stored in the *Parser.Records struct filed
-		err = csvparse.NewEncoder(os.Stdout).EncodeCSV(data)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+	for _, z := range x {
+		fmt.Println(z)
 	}
+
+	// if *j {
+	// 	// MarshalIndent outputs []*Record to JSON for output to os.Stdout
+	// 	// []*Record are stored in the *Parser.Records struct filed
+	// 	err := csvparse.NewEncoder(os.Stdout).EncodeJSON(data)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		os.Exit(1)
+	// 	}
+	// } else {
+	// 	// MarshaltoCSV marshals []*Record to []string for output to os.Stdout
+	// 	// []*Record are stored in the *Parser.Records struct filed
+	// 	err = csvparse.NewEncoder(os.Stdout).EncodeCSV(data)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		os.Exit(1)
+	// 	}
+	// }
 }
--- parse.go
+++ parse.go
@@ -2,7 +2,6 @@ package csvparse
 
 import (
 	"encoding/csv"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -14,111 +13,126 @@ import (
 	"github.com/blendlabs/go-name-parser"
 )
 
-// Record represents a customer
-type Record struct {
-	Fullname   string    `json:"Full_name" csv:"(?i)^fullname$" fmt:"tc"`
-	Firstname  string    `json:"First_name" csv:"(?i)^first[ _-]?name$" fmt:"tc"`
-	MI         string    `json:"Middle_name" csv:"(?i)^mi$" fmt:"uc"`
-	Lastname   string    `json:"Last_name" csv:"(?i)^last[ _-]?name$" fmt:"tc"`
-	Address1   string    `json:"Address_1" csv:"(?i)^address[ _-]?1?$" fmt:"tc"`
-	Address2   string    `json:"Address_2" csv:"(?i)^address[ _-]?2$" fmt:"tc"`
-	City       string    `json:"City" csv:"(?i)^city$" fmt:"tc"`
-	State      string    `json:"State" csv:"(?i)^state$|^st$" fmt:"uc"`
-	Zip        string    `json:"Zip" csv:"(?i)^(zip|postal)[ _]?(code)?$" fmt:"uc"`
-	Zip4       string    `json:"Zip_4" csv:"(?i)^zip4$|^4zip$" fmt:"uc"`
-	HPH        string    `json:"Home_phone" csv:"(?i)^hph$|^home[ _]phone$" fmt:"fp"`
-	BPH        string    `json:"Business_phone" csv:"(?i)^bph$|^(work|business)[ _]phone$" fmt:"fp"`
-	CPH        string    `json:"Mobile_phone" csv:"(?i)^cph$|^mobile[ _]phone$" fmt:"fp"`
-	Email      string    `json:"Email" csv:"(?i)^email[ _]?(address)?$" fmt:"lc"`
-	VIN        string    `json:"VIN" csv:"(?i)^vin$" fmt:"-"`
-	Year       string    `json:"Year" csv:"(?i)^year$|^vyr$" fmt:"-"`
-	Make       string    `json:"Make" csv:"(?i)^make$|^vmk$" fmt:"tc"`
-	Model      string    `json:"Model" csv:"(?i)^model$|^vmd$" fmt:"tc"`
-	DelDate    time.Time `json:"Delivery_date" csv:"(?i)^del[ ]?date$" fmt:"-"`
-	Date       time.Time `json:"Last_service_date" csv:"(?i)^date$" fmt:"-"`
-	DSFwalkseq string    `json:"DSF_Walk_Sequence" csv:"(?i)^DSF_WALK_SEQ$" fmt:"uc"`
-	CRRT       string    `json:"CRRT" csv:"(?i)^crrt$" fmt:"uc"`
-	KBB        string    `json:"KBB" csv:"(?i)^kbb$" fmt:"uc"`
-}
-
 // CSVDecoder holds the header field map and reader
 type CSVDecoder struct {
-	header  map[string]int
-	file    io.Reader
-	records []Record
+	header map[string]int
+	file   io.Reader
+	// records []Record
 }
 
 // NewDecoder initializes a new parser
 func NewDecoder(input io.Reader) *CSVDecoder {
 	return &CSVDecoder{
-		header:  map[string]int{},
-		file:    input,
-		records: []Record{},
+		header: map[string]int{},
+		file:   input,
+		// records: []Record{},
 	}
 }
 
 // DecodeCSV unmarshalls CSV file to record struct
-func (d *CSVDecoder) DecodeCSV() ([]Record, error) {
+func (d *CSVDecoder) DecodeCSV(v interface{}) error {
 	csvRows, err := getCSVRows(d.file)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	if len(csvRows) == 0 {
-		return nil, errors.New("empty csv file given")
+		return errors.New("empty csv file given")
 	}
 	headerRow := csvRows[0]
 	body := csvRows[1:]
 
 	if err := checkForDoubleHeaderNames(headerRow); err != nil {
-		return nil, err
+		return err
 	}
 
-	r := Record{}
-	sValue := reflect.ValueOf(&r)
-	sLen := sValue.Elem().NumField()
+	slice := checkifSlice(v)
+	if slice.Kind() != reflect.Slice {
+		return errors.New("Need to pass in a slice")
+	}
+	innerType := getInnerSliceType(v)
+	if innerType.Kind() != reflect.Struct {
+		return errors.New("Need to pass in a slice of stucts")
+	}
 
+	innerValueHdr := reflect.New(innerType)
+	innerValueHdrLen := innerValueHdr.Elem().NumField()
 	for i, csvColumnHdr := range headerRow {
-		for j := 0; j < sLen; j++ {
-			regex := reflect.Indirect(sValue).Type().Field(j).Tag.Get("csv")
+		for j := 0; j < innerValueHdrLen; j++ {
+			regex := reflect.Indirect(innerValueHdr).Type().Field(j).Tag.Get("csv")
 			if regexp.MustCompile(regex).MatchString(csvColumnHdr) {
-				d.header[reflect.Indirect(sValue).Type().Field(j).Name] = i
+				d.header[reflect.Indirect(innerValueHdr).Type().Field(j).Name] = i
 			}
 		}
 	}
 
 	for _, csvRow := range body {
-		for j := 0; j < sLen; j++ {
-			sFName := reflect.Indirect(sValue).Type().Field(j).Name
-			switch sValue.Elem().Field(j).Type() {
+		innerValueRow := reflect.New(innerType)
+		innerValueRowLen := innerValueRow.Elem().NumField()
+		for j := 0; j < innerValueRowLen; j++ {
+			sFName := reflect.Indirect(innerValueRow).Type().Field(j).Name
+			switch innerValueRow.Elem().Type().Field(j).Type {
 			case reflect.TypeOf(""):
 				if _, ok := d.header[sFName]; ok {
-					format := reflect.Indirect(sValue).Type().Field(j).Tag.Get("fmt")
+					format := reflect.Indirect(innerValueRow).Type().Field(j).Tag.Get("fmt")
 					val := formatStringVals(sFName, format, csvRow[d.header[sFName]])
-					sValue.Elem().FieldByName(sFName).Set(reflect.ValueOf(val))
+					innerValueRow.Elem().FieldByName(sFName).Set(reflect.ValueOf(val))
 				}
 			case reflect.TypeOf(time.Now()):
 				if _, ok := d.header[sFName]; ok {
 					val := ParseDate(csvRow[d.header[sFName]])
-					sValue.Elem().FieldByName(sFName).Set(reflect.ValueOf(val))
+					innerValueRow.Elem().FieldByName(sFName).Set(reflect.ValueOf(val))
+				}
+			default:
+				if _, ok := d.header[sFName]; ok {
+					val := csvRow[d.header[sFName]]
+					innerValueRow.Elem().FieldByName(sFName).Set(reflect.ValueOf(val))
 				}
 			}
-			if j == sLen-1 {
-				if r.Fullname != "" && (r.Firstname == "" || r.Lastname == "") {
-					name := names.Parse(r.Fullname)
-					r.Firstname = TCase(name.FirstName)
-					r.MI = UCase(name.MiddleName)
-					r.Lastname = TCase(name.LastName)
+			if j == innerValueRowLen-1 {
+				fullname := innerValueRow.Elem().FieldByName("Fullname")
+				fname := innerValueRow.Elem().FieldByName("Firstname")
+				lname := innerValueRow.Elem().FieldByName("Lastname")
+				if fullname.IsValid() && fname.IsValid() && lname.IsValid() {
+					if fullname.String() != "" && (fname.String() == "" || lname.String() == "") {
+						name := names.Parse(fullname.String())
+						innerValueRow.Elem().FieldByName("Firstname").Set(reflect.ValueOf(name.FirstName))
+						innerValueRow.Elem().FieldByName("MI").Set(reflect.ValueOf(name.MiddleName))
+						innerValueRow.Elem().FieldByName("Lastname").Set(reflect.ValueOf(name.LastName))
+					}
 				}
-				zip, zip4 := ParseZip(r.Zip)
-				r.Zip = zip
-				if zip4 != "" {
-					r.Zip4 = zip4
+				zip := innerValueRow.Elem().FieldByName("Zip")
+				if zip.IsValid() {
+					zip, zip4 := ParseZip(zip.String())
+					innerValueRow.Elem().FieldByName("Zip").Set(reflect.ValueOf(zip))
+					if zip4 != "" {
+						innerValueRow.Elem().FieldByName("Zip4").Set(reflect.ValueOf(zip4))
+					}
 				}
 			}
 		}
-		d.records = append(d.records, r)
+		slice.Set(reflect.Append(slice, innerValueRow))
 	}
-	return d.records, nil
+	return nil
+}
+
+func getInnerSliceType(v interface{}) reflect.Type {
+	outType := reflect.TypeOf(v)
+	if outType.Kind() == reflect.Ptr {
+		outType = outType.Elem()
+	}
+	inType := outType.Elem()
+	if inType.Kind() == reflect.Ptr {
+		inType = inType.Elem()
+	}
+	return inType
+}
+
+func checkifSlice(v interface{}) reflect.Value {
+	value := reflect.ValueOf(v)
+	if value.Kind() == reflect.Ptr {
+		value = value.Elem()
+	}
+	return value
 }
 
 func getCSVRows(r io.Reader) ([][]string, error) {
@@ -157,67 +171,67 @@ func formatStringVals(name, format, val string) string {
 	return val
 }
 
-// CSVEncoder outputs JSON values to an output stream.
-type CSVEncoder struct {
-	output io.Writer
-}
+// // CSVEncoder outputs JSON values to an output stream.
+// type CSVEncoder struct {
+// 	output io.Writer
+// }
 
-// NewEncoder initializes a new parser
-func NewEncoder(output io.Writer) *CSVEncoder {
-	return &CSVEncoder{output: output}
-}
+// // NewEncoder initializes a new parser
+// func NewEncoder(output io.Writer) *CSVEncoder {
+// 	return &CSVEncoder{output: output}
+// }
 
-// EncodeCSV marshalls the Record struct then outputs to csv
-func (e *CSVEncoder) EncodeCSV(Records []Record) error {
-	wtr := csv.NewWriter(e.output)
+// // EncodeCSV marshalls the Record struct then outputs to csv
+// func (e *CSVEncoder) EncodeCSV(Records []Record) error {
+// 	wtr := csv.NewWriter(e.output)
 
-	var header []string
-	headerLen := reflect.ValueOf(&Records[0]).Elem().NumField()
+// 	var header []string
+// 	headerLen := reflect.ValueOf(&Records[0]).Elem().NumField()
 
-	for i := 0; i < headerLen; i++ {
-		headerName := reflect.Indirect(reflect.ValueOf(&Records[0])).Type().Field(i).Name
-		header = append(header, headerName)
-	}
+// 	for i := 0; i < headerLen; i++ {
+// 		headerName := reflect.Indirect(reflect.ValueOf(&Records[0])).Type().Field(i).Name
+// 		header = append(header, headerName)
+// 	}
 
-	if err := wtr.Write(header); err != nil {
-		return fmt.Errorf("error writing header to csv: %v", err)
-	}
+// 	if err := wtr.Write(header); err != nil {
+// 		return fmt.Errorf("error writing header to csv: %v", err)
+// 	}
 
-	for _, r := range Records {
-		var row []string
-		rowLen := reflect.ValueOf(r).NumField()
-		for i := 0; i < rowLen; i++ {
-			val := fmt.Sprint(reflect.ValueOf(r).Field(i))
-			switch reflect.Indirect(reflect.ValueOf(Records[0])).Type().Field(i).Name {
-			case "DelDate", "Date":
-				if !r.DelDate.IsZero() {
-					val = fmt.Sprintf("%v/%v/%v", int(r.DelDate.Month()), r.DelDate.Day(), r.DelDate.Year())
-				} else {
-					val = ""
-				}
-			}
-			row = append(row, val)
-		}
-		if err := wtr.Write(row); err != nil {
-			return fmt.Errorf("error writing row to csv: %v", err)
-		}
-	}
-	wtr.Flush()
-	if err := wtr.Error(); err != nil {
-		return fmt.Errorf("error writing to output: %v", err)
-	}
-	return nil
-}
+// 	for _, r := range Records {
+// 		var row []string
+// 		rowLen := reflect.ValueOf(r).NumField()
+// 		for i := 0; i < rowLen; i++ {
+// 			val := fmt.Sprint(reflect.ValueOf(r).Field(i))
+// 			switch reflect.Indirect(reflect.ValueOf(Records[0])).Type().Field(i).Name {
+// 			case "DelDate", "Date":
+// 				if !r.DelDate.IsZero() {
+// 					val = fmt.Sprintf("%v/%v/%v", int(r.DelDate.Month()), r.DelDate.Day(), r.DelDate.Year())
+// 				} else {
+// 					val = ""
+// 				}
+// 			}
+// 			row = append(row, val)
+// 		}
+// 		if err := wtr.Write(row); err != nil {
+// 			return fmt.Errorf("error writing row to csv: %v", err)
+// 		}
+// 	}
+// 	wtr.Flush()
+// 	if err := wtr.Error(); err != nil {
+// 		return fmt.Errorf("error writing to output: %v", err)
+// 	}
+// 	return nil
+// }
 
-// EncodeJSON marshalls the Record struct then outputs to Indented JSON
-func (e *CSVEncoder) EncodeJSON(Records []Record) error {
-	data, err := json.MarshalIndent(Records, " ", " ")
-	if err != nil {
-		return fmt.Errorf("error encoding to JSON output: %v", err)
-	}
-	fmt.Fprintln(e.output, string(data))
-	return nil
-}
+// // EncodeJSON marshalls the Record struct then outputs to Indented JSON
+// func (e *CSVEncoder) EncodeJSON(Records []Record) error {
+// 	data, err := json.MarshalIndent(Records, " ", " ")
+// 	if err != nil {
+// 		return fmt.Errorf("error encoding to JSON output: %v", err)
+// 	}
+// 	fmt.Fprintln(e.output, string(data))
+// 	return nil
+// }
 
 // TCase transforms string to title case and trims leading & trailing white space
 func TCase(f string) string {
@@ -236,6 +250,9 @@ func LCase(f string) string {
 
 // ParseZip perses ZIP code to Zip & Zip4
 func ParseZip(zip string) (string, string) {
+	if zip == "" {
+		return "", ""
+	}
 	switch {
 	case regexp.MustCompile(`(?i)^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$`).MatchString(zip):
 		return TrimZeros(zip[:5]), TrimZeros(zip[5:])
