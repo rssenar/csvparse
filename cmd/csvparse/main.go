60187c4c0b719c6f63017168d0da7c00d44c945f
Richard Senar <richardsenar@richards-imac.lan>
2018-04-07 08:06:58 -0700

    updated csv output func

--- cmd/csvparse/main.go
+++ cmd/csvparse/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"reflect"
+	"sync"
 	"time"
 
-	"github.com/rssenar/csvparse"
+	"github.com/blendlabs/go-name-parser"
+	cp "github.com/rssenar/csvparse"
 )
 
 type client struct {
@@ -36,11 +41,11 @@ type client struct {
 }
 
 func main() {
+	defer timeTrack(time.Now(), "CSVParser")
 	flag.Parse()
-
-	var input io.Reader
 	args := flag.Args()
 
+	var input io.Reader
 	if len(args) != 0 {
 		// verify if file was passed through as a command-line argument
 		if len(args) > 1 {
@@ -67,8 +72,134 @@ func main() {
 	// Pass in black []*Record{} container to be filled
 	data := []*client{}
 
-	err := csvparse.NewDecoder(input).DecodeCSV(&data)
+	err := cp.NewDecoder(input).DecodeCSV(&data)
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+type csvEncoder struct{ output io.Writer }
+
+// NewEncoder initializes a new output
+func newEncoder(output io.Writer) *csvEncoder { return &csvEncoder{output: output} }
+
+// EncodeCSV marshalls the Record struct then outputs to csv
+func (e *csvEncoder) encodeCSV(data []*client, concurency *int) error {
+	defer timeTrack(time.Now(), "encodeCSV")
+
+	var Client *client
+
+	tasks := make(chan *client)
+	go func() {
+		for _, Client = range data {
+			tasks <- Client
+		}
+		close(tasks)
+	}()
+
+	results := make(chan []string)
+	var wg sync.WaitGroup
+
+	wg.Add(*concurency)
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for i := 0; i < *concurency; i++ {
+		go func() {
+			defer wg.Done()
+			for t := range tasks {
+				r, err := formatStructFields(t)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				results <- r
+			}
+		}()
+	}
+	if err := outputCSV(e.output, results, Client); err != nil {
+		log.Printf("could not write to %s: %v", e.output, err)
+	}
+	return nil
+}
+
+func outputCSV(output io.Writer, records <-chan []string, c *client) error {
+	w := csv.NewWriter(output)
+
+	var header []string
+	sValue := reflect.ValueOf(c).Elem()
+	for i := 0; i < sValue.NumField(); i++ {
+		name := reflect.Indirect(sValue).Type().Field(i).Name
+		header = append(header, name)
+	}
+	if err := w.Write(header); err != nil {
+		log.Fatalf("could not write header to csv: %v", err)
+	}
+
+	for r := range records {
+		if err := w.Write(r); err != nil {
+			log.Fatalf("could not write record to csv: %v", err)
+		}
+	}
+	w.Flush()
+	if err := w.Error(); err != nil {
+		return fmt.Errorf("writer failed: %v", err)
+	}
+	return nil
+}
+
+func formatStructFields(t *client) ([]string, error) {
+	if t.Fullname != "" && (t.Firstname == "" || t.Lastname == "") {
+		name := names.Parse(t.Fullname)
+		t.Firstname = name.FirstName
+		t.MI = name.MiddleName
+		t.Lastname = name.LastName
+	}
+	if t.Zip != "" {
+		zip, zip4 := cp.ParseZip(t.Zip)
+		t.Zip = zip
+		if zip4 != "" {
+			t.Zip4 = zip4
+		}
+	}
+
+	var row []string
+	sValue := reflect.ValueOf(t).Elem()
+	for i := 0; i < sValue.NumField(); i++ {
+		var value string
+		name := reflect.Indirect(sValue).Type().Field(i).Name
+		switch sValue.Field(i).Type() {
+		case reflect.TypeOf(time.Now()):
+			time := fmt.Sprint(sValue.FieldByName(name))[:10]
+			if time == "0001-01-01" {
+				time = ""
+			}
+			value = time
+		default:
+			if format, ok := reflect.Indirect(sValue).Type().Field(i).Tag.Lookup("fmt"); ok {
+				switch format {
+				case "-":
+					value = fmt.Sprint(sValue.FieldByName(name))
+				default:
+					fmtvalue, err := cp.FormatStringVals(format, fmt.Sprint(sValue.FieldByName(name)))
+					if err != nil {
+						return nil, err
+					}
+					value = fmtvalue
+				}
+			} else {
+				value = fmt.Sprint(sValue.FieldByName(name))
+			}
+		}
+		row = append(row, value)
+	}
+	return row, nil
+}
+
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", name, elapsed)
 }
