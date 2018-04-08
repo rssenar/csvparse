2e966e33e26033ec9a0a4601d82d6a37b5acdfc8
Richard Senar <richardsenar@richards-imac.lan>
2018-04-07 20:58:03 -0700

    modified client struct fields

--- cmd/csvparse/main.go
+++ cmd/csvparse/main.go
@@ -16,19 +16,28 @@ import (
 )
 
 type client struct {
-	Fullname  string    `json:"Full_name" csv:"(?i)^fullname$" fmt:"tc"`
-	Firstname string    `json:"First_name" csv:"(?i)^first[ _-]?name$" fmt:"tc"`
-	MI        string    `json:"Middle_name" csv:"(?i)^mi$" fmt:"uc"`
-	Lastname  string    `json:"Last_name" csv:"(?i)^last[ _-]?name$" fmt:"tc"`
-	Address1  string    `json:"Address_1" csv:"(?i)^address[ _-]?1?$" fmt:"tc"`
-	Address2  string    `json:"Address_2" csv:"(?i)^address[ _-]?2$" fmt:"tc"`
-	City      string    `json:"City" csv:"(?i)^city$" fmt:"tc"`
-	State     string    `json:"State" csv:"(?i)^state$|^st$" fmt:"uc"`
-	Zip       string    `json:"Zip" csv:"(?i)^(zip|postal)[ _]?(code)?$" fmt:"-"`
-	Zip4      string    `json:"Zip_4" csv:"(?i)^zip4$|^4zip$" fmt:"-"`
-	HPH       string    `json:"Home_phone" csv:"(?i)^hph$|^home[ _]phone$" fmt:"fp"`
-	Email     string    `json:"Email" csv:"(?i)^email[ _]?(address)?$" fmt:"lc"`
-	Date      time.Time `json:"Last_service_date" csv:"(?i)^date$" fmt:"-"`
+	Fullname   string    `json:"Full_name" csv:"(?i)^fullname$" fmt:"tc"`
+	Firstname  string    `json:"First_name" csv:"(?i)^first[ _-]?name$" fmt:"tc"`
+	MI         string    `json:"Middle_name" csv:"(?i)^mi$" fmt:"uc"`
+	Lastname   string    `json:"Last_name" csv:"(?i)^last[ _-]?name$" fmt:"tc"`
+	Address1   string    `json:"Address_1" csv:"(?i)^address[ _-]?1?$" fmt:"tc"`
+	Address2   string    `json:"Address_2" csv:"(?i)^address[ _-]?2$" fmt:"tc"`
+	City       string    `json:"City" csv:"(?i)^city$" fmt:"tc"`
+	State      string    `json:"State" csv:"(?i)^state$|^st$" fmt:"uc"`
+	Zip        string    `json:"Zip" csv:"(?i)^(zip|postal)[ _]?(code)?$" fmt:"-"`
+	Zip4       string    `json:"Zip_4" csv:"(?i)^zip4$|^4zip$" fmt:"-"`
+	CRRT       string    `json:"CRRT" csv:"(?i)^crrt$" fmt:"uc"`
+	DSFwalkseq string    `json:"DSF_Walk_Sequence" csv:"(?i)^DSF_WALK_SEQ$" fmt:"uc"`
+	HPH        string    `json:"Home_phone" csv:"(?i)^hph$|^home[ _]phone$" fmt:"fp"`
+	BPH        string    `json:"Business_phone" csv:"(?i)^bph$|^(work|business)[ _]phone$" fmt:"fp"`
+	CPH        string    `json:"Mobile_phone" csv:"(?i)^cph$|^mobile[ _]phone$" fmt:"fp"`
+	Email      string    `json:"Email" csv:"(?i)^email[ _]?(address)?$" fmt:"lc"`
+	VIN        string    `json:"VIN" csv:"(?i)^vin$" fmt:"uc"`
+	Year       string    `json:"Veh_Year" csv:"(?i)^year$|^vyr$" fmt:"-"`
+	Make       string    `json:"Veh_Make" csv:"(?i)^make$|^vmk$" fmt:"tc"`
+	Model      string    `json:"Veh_Model" csv:"(?i)^model$|^vmd$" fmt:"tc"`
+	DelDate    time.Time `json:"Delivery_date" csv:"(?i)^del[ ]?date$" fmt:"-"`
+	Date       time.Time `json:"Last_service_date" csv:"(?i)^date$" fmt:"-"`
 }
 
 func main() {
